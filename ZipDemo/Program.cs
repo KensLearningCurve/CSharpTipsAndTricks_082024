273c52c894f30cd9eba0453f3b006c000e6dd889
info@kenslearningcurve.com <info@kenslearningcurve.com>
2024-07-29 09:23:40 +0200

    Adding logic and Zip method

---
+++ ZipDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Logic;
+
+IEnumerable<string> productTitles = ProductList.Products.Take(10).Select(x => x.Title);
+IEnumerable<int> productStocks = ProductList.Products.Take(10).Select(x => x.Stock);
+
+var products = productTitles.Zip(productStocks, (title, stock) => new
+{
+    Title = title,
+    Stock = stock
+});
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Title} - {product.Stock}");
+}
