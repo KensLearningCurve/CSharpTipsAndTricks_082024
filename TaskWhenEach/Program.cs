09fd652dd908eae6688ebac04184b77cc076a9e3
info@kenslearningcurve.com <info@kenslearningcurve.com>
2024-07-29 09:44:12 +0200

    Adding Task.WhenEach

---
+++ TaskWhenEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using Logic;
+
+List<Task<(bool, Product)>> downloadTasks = ProductList.Products
+    .GetRange(1, 10)
+    .Select(ShouldBeOrdered)
+    .ToList();
+
+await foreach (var finishedTask in Task.WhenEach(downloadTasks))
+{
+    (bool order, Product product) result = await finishedTask;
+    Console.WriteLine($"{result.product.Title} -> {result.order}");
+}
+
+async Task<(bool, Product)> ShouldBeOrdered(Product product)
+{
+    bool result = product.Stock < 10 && product.Status == Status.Delivered;
+
+    await Task.Delay(Random.Shared.Next(1, 1000));
+
+    return (result, product);
+}
