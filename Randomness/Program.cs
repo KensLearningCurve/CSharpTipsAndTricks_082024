40e75b1e5987f0337eabd58f54088f8c497cd3f4
info@kenslearningcurve.com <info@kenslearningcurve.com>
2024-08-05 09:03:06 +0200

    Adding randomness

---
+++ Randomness/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Logic;
+
+Product[] products = Random.Shared.GetItems(ProductList.Products.ToArray(), 5);
+Console.WriteLine("GetItems<T>:");
+foreach (Product product in products)
+{
+    Console.WriteLine($"Name: {product.Title}, Stock: {product.Stock}");
+}
+
+Product[] forShuffle = ProductList.Products.ToArray();
+Random.Shared.Shuffle(forShuffle);
+Console.WriteLine(string.Empty);
+Console.WriteLine("Shuffle<T>:");
+foreach (Product product in forShuffle)
+{
+    Console.WriteLine($"Name: {product.Title}, Stock: {product.Stock}");
+}
