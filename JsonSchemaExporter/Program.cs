b32309d7ff57ae53975896420e249c14ac4d3777
info@kenslearningcurve.com <info@kenslearningcurve.com>
2024-08-05 08:51:58 +0200

    Adding JsonSchemaExporter

---
+++ JsonSchemaExporter/Program.cs
@@ -0,0 +1,10 @@
+﻿using Logic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+
+
+JsonNode jsonSchema = JsonSchemaExporter
+    .GetJsonSchemaAsNode(JsonSerializerOptions.Default, typeof(Product));
+
+Console.WriteLine(jsonSchema);
